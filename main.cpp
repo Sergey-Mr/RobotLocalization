dbf4cc5fff6b7a0068bb7d12b4c9304a9b775da4
Serhii <sergey.st265@gmail.com>
2025-01-26 22:57:35 +0000

    made UI better

--- main.cpp
+++ main.cpp
@@ -2,126 +2,107 @@
 #include "Map.h"
 #include "Robot.h"
 #include <iostream>
+#include <sstream>
 
 const int CELL_SIZE = 50;
+const int WINDOW_WIDTH = 1024;
+const int WINDOW_HEIGHT = 768;
 const char* FONT_PATH = "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf";
+
+// Add hover effect for buttons
+sf::RectangleShape createButton(float x, float y, const sf::Vector2f& size, bool isHovered) {
+    sf::RectangleShape button(size);
+    button.setPosition(x, y);
+    if (isHovered) {
+        button.setFillColor(sf::Color(220, 220, 220));
+    } else {
+        button.setFillColor(sf::Color::White);
+    }
+    button.setOutlineThickness(2);
+    button.setOutlineColor(sf::Color(100, 100, 100));
+    return button;
+}
 
 void drawMap(sf::RenderWindow& window, const Map& map, const Robot& robot) {
     const auto& grid = map.getGrid();
     for (int y = 0; y < grid.size(); ++y) {
         for (int x = 0; x < grid[0].size(); ++x) {
             sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-            cell.setPosition(x * CELL_SIZE, y * CELL_SIZE);
-            cell.setFillColor(sf::Color::Black);
+            cell.setPosition(x * CELL_SIZE + 50, y * CELL_SIZE + 50);
+            cell.setFillColor(sf::Color(20, 20, 20));
             cell.setOutlineThickness(1);
-            cell.setOutlineColor(sf::Color::White);
+            cell.setOutlineColor(sf::Color(50, 50, 50));
             window.draw(cell);
 
             if (grid[y][x] == 1) {
                 sf::RectangleShape object(sf::Vector2f(CELL_SIZE, CELL_SIZE));
-                object.setPosition(x * CELL_SIZE, y * CELL_SIZE);
+                object.setPosition(x * CELL_SIZE + 50, y * CELL_SIZE + 50);
                 object.setFillColor(sf::Color::White);
-                object.setOutlineThickness(1);
-                object.setOutlineColor(sf::Color::White);
+                object.setOutlineThickness(2);
+                object.setOutlineColor(sf::Color(200, 200, 200));
                 window.draw(object);
             }
         }
     }
 
-    // Draw the robot as a circle
     sf::CircleShape robotShape(CELL_SIZE / 2);
-    robotShape.setPosition(robot.getX() * CELL_SIZE, robot.getY() * CELL_SIZE);
-    robotShape.setFillColor(sf::Color::Red);
-    robotShape.setOutlineThickness(1);
-    robotShape.setOutlineColor(sf::Color::White);
+    robotShape.setPosition(robot.getX() * CELL_SIZE + 50, robot.getY() * CELL_SIZE + 50);
+    robotShape.setFillColor(sf::Color(255, 50, 50));
+    robotShape.setOutlineThickness(2);
+    robotShape.setOutlineColor(sf::Color(200, 0, 0));
     window.draw(robotShape);
 }
 
-void drawButtons(sf::RenderWindow& window, const sf::Font& font) {
-    // Up button
-    sf::RectangleShape upButton(sf::Vector2f(100, 50));
-    upButton.setPosition(550, 50);
-    upButton.setFillColor(sf::Color::White);
-    upButton.setOutlineThickness(1);
-    upButton.setOutlineColor(sf::Color::Black);
-    window.draw(upButton);
+void drawUI(sf::RenderWindow& window, const sf::Font& font, const std::vector<int>& sensorReadings, const sf::Vector2i& mousePos) {
+    sf::Text title("Robot Localization Simulator", font, 30);
+    title.setFillColor(sf::Color::White);
+    title.setPosition(20, 10);
+    window.draw(title);
 
-    sf::Text upText("UP", font, 20);
-    upText.setFillColor(sf::Color::Black);
-    sf::FloatRect textRect = upText.getLocalBounds();
-    upText.setOrigin(textRect.left + textRect.width/2.0f,
-                     textRect.top + textRect.height/2.0f);
-    upText.setPosition(upButton.getPosition().x + upButton.getSize().x/2.0f,
-                      upButton.getPosition().y + upButton.getSize().y/2.0f);
-    window.draw(upText);
+    // Calculate center position for the joystick layout
+    float centerX = WINDOW_WIDTH - 300;
+    float centerY = 250;
+    float buttonWidth = 100;
+    float buttonHeight = 50;
+    float spacing = 60;
 
-    // Down button
-    sf::RectangleShape downButton(sf::Vector2f(100, 50));
-    downButton.setPosition(550, 150);
-    downButton.setFillColor(sf::Color::White);
-    downButton.setOutlineThickness(1);
-    downButton.setOutlineColor(sf::Color::Black);
-    window.draw(downButton);
+    std::vector<std::pair<sf::RectangleShape, std::string>> buttons = {
+        {createButton(centerX, centerY - spacing, sf::Vector2f(buttonWidth, buttonHeight), false), "UP"},
+        {createButton(centerX, centerY + spacing, sf::Vector2f(buttonWidth, buttonHeight), false), "DOWN"},
+        {createButton(centerX - spacing, centerY, sf::Vector2f(buttonWidth, buttonHeight), false), "LEFT"},
+        {createButton(centerX + spacing, centerY, sf::Vector2f(buttonWidth, buttonHeight), false), "RIGHT"},
+        {createButton(centerX, centerY + spacing * 2.5, sf::Vector2f(buttonWidth, buttonHeight), false), "SENSOR"}
+    };
 
-    sf::Text downText("DOWN", font, 20);
-    downText.setFillColor(sf::Color::Black);
-    textRect = downText.getLocalBounds();
-    downText.setOrigin(textRect.left + textRect.width/2.0f,
-                      textRect.top + textRect.height/2.0f);
-    downText.setPosition(downButton.getPosition().x + downButton.getSize().x/2.0f,
-                        downButton.getPosition().y + downButton.getSize().y/2.0f);
-    window.draw(downText);
+    for (const auto& button : buttons) {
+        window.draw(button.first);
+        sf::Text text(button.second, font, 20);
+        text.setFillColor(sf::Color::Black);
+        sf::FloatRect textRect = text.getLocalBounds();
+        text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
+        text.setPosition(button.first.getPosition().x + button.first.getSize().x/2.0f,
+                        button.first.getPosition().y + button.first.getSize().y/2.0f);
+        window.draw(text);
+    }
 
-    // Left button
-    sf::RectangleShape leftButton(sf::Vector2f(100, 50));
-    leftButton.setPosition(450, 100);
-    leftButton.setFillColor(sf::Color::White);
-    leftButton.setOutlineThickness(1);
-    leftButton.setOutlineColor(sf::Color::Black);
-    window.draw(leftButton);
+    if (!sensorReadings.empty()) {
+        sf::RectangleShape sensorPanel(sf::Vector2f(300, 100));
+        sensorPanel.setPosition(WINDOW_WIDTH - 350, WINDOW_HEIGHT - 150);
+        sensorPanel.setFillColor(sf::Color(40, 40, 40));
+        sensorPanel.setOutlineThickness(2);
+        sensorPanel.setOutlineColor(sf::Color(100, 100, 100));
+        window.draw(sensorPanel);
 
-    sf::Text leftText("LEFT", font, 20);
-    leftText.setFillColor(sf::Color::Black);
-    textRect = leftText.getLocalBounds();
-    leftText.setOrigin(textRect.left + textRect.width/2.0f,
-                      textRect.top + textRect.height/2.0f);
-    leftText.setPosition(leftButton.getPosition().x + leftButton.getSize().x/2.0f,
-                        leftButton.getPosition().y + leftButton.getSize().y/2.0f);
-    window.draw(leftText);
-
-    // Right button
-    sf::RectangleShape rightButton(sf::Vector2f(100, 50));
-    rightButton.setPosition(650, 100);
-    rightButton.setFillColor(sf::Color::White);
-    rightButton.setOutlineThickness(1);
-    rightButton.setOutlineColor(sf::Color::Black);
-    window.draw(rightButton);
-
-    sf::Text rightText("RIGHT", font, 20);
-    rightText.setFillColor(sf::Color::Black);
-    textRect = rightText.getLocalBounds();
-    rightText.setOrigin(textRect.left + textRect.width/2.0f,
-                       textRect.top + textRect.height/2.0f);
-    rightText.setPosition(rightButton.getPosition().x + rightButton.getSize().x/2.0f,
-                         rightButton.getPosition().y + rightButton.getSize().y/2.0f);
-    window.draw(rightText);
-
-    // Sensor button
-    sf::RectangleShape sensorButton(sf::Vector2f(100, 50));
-    sensorButton.setPosition(550, 250);
-    sensorButton.setFillColor(sf::Color::White);
-    sensorButton.setOutlineThickness(1);
-    sensorButton.setOutlineColor(sf::Color::Black);
-    window.draw(sensorButton);
-
-    sf::Text sensorText("SENSOR", font, 20);
-    sensorText.setFillColor(sf::Color::Black);
-    textRect = sensorText.getLocalBounds();
-    sensorText.setOrigin(textRect.left + textRect.width/2.0f,
-                        textRect.top + textRect.height/2.0f);
-    sensorText.setPosition(sensorButton.getPosition().x + sensorButton.getSize().x/2.0f,
-                          sensorButton.getPosition().y + sensorButton.getSize().y/2.0f);
-    window.draw(sensorText);
+        std::stringstream ss;
+        ss << "Sensor Readings:\n";
+        for (int reading : sensorReadings) {
+            ss << reading << " ";
+        }
+        sf::Text sensorText(ss.str(), font, 16);
+        sensorText.setFillColor(sf::Color::White);
+        sensorText.setPosition(WINDOW_WIDTH - 340, WINDOW_HEIGHT - 140);
+        window.draw(sensorText);
+    }
 }
 
 bool isButtonClicked(const sf::RectangleShape& button, const sf::Vector2i& mousePos) {
@@ -134,7 +115,8 @@ int main() {
     map.addObject(5, 5, 2, 3);
     Robot robot(1, 1, map, 0.1);
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Robot Localization");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Robot Localization");
+    window.setFramerateLimit(60);
     
     sf::Font font;
     if (!font.loadFromFile(FONT_PATH)) {
@@ -142,46 +124,47 @@ int main() {
         return -1;
     }
 
+    std::vector<int> currentSensorReadings;
+
     while (window.isOpen()) {
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             } else if (event.type == sf::Event::MouseButtonPressed) {
                 sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-                sf::RectangleShape upButton(sf::Vector2f(100, 50));
-                upButton.setPosition(550, 50);
-                sf::RectangleShape downButton(sf::Vector2f(100, 50));
-                downButton.setPosition(550, 150);
-                sf::RectangleShape leftButton(sf::Vector2f(100, 50));
-                leftButton.setPosition(450, 100);
-                sf::RectangleShape rightButton(sf::Vector2f(100, 50));
-                rightButton.setPosition(650, 100);
-                sf::RectangleShape sensorButton(sf::Vector2f(100, 50));
-                sensorButton.setPosition(550, 250);
+                
+                std::vector<sf::RectangleShape> buttons;
+                float centerX = WINDOW_WIDTH - 300;
+                float centerY = 250;
+                float buttonWidth = 100;
+                float buttonHeight = 50;
+                float spacing = 60;
 
-                if (isButtonClicked(upButton, mousePos)) {
+                buttons.push_back(createButton(centerX, centerY - spacing, sf::Vector2f(buttonWidth, buttonHeight), false));
+                buttons.push_back(createButton(centerX, centerY + spacing, sf::Vector2f(buttonWidth, buttonHeight), false));
+                buttons.push_back(createButton(centerX - spacing, centerY, sf::Vector2f(buttonWidth, buttonHeight), false));
+                buttons.push_back(createButton(centerX + spacing, centerY, sf::Vector2f(buttonWidth, buttonHeight), false));
+                buttons.push_back(createButton(centerX, centerY + spacing * 2.5, sf::Vector2f(buttonWidth, buttonHeight), false));
+                if (isButtonClicked(buttons[0], mousePos)) {
                     robot.moveUp();
-                } else if (isButtonClicked(downButton, mousePos)) {
+                } else if (isButtonClicked(buttons[1], mousePos)) {
                     robot.moveDown();
-                } else if (isButtonClicked(leftButton, mousePos)) {
+                } else if (isButtonClicked(buttons[2], mousePos)) {
                     robot.moveLeft();
-                } else if (isButtonClicked(rightButton, mousePos)) {
+                } else if (isButtonClicked(buttons[3], mousePos)) {
                     robot.moveRight();
-                } else if (isButtonClicked(sensorButton, mousePos)) {
-                    std::vector<int> readings = robot.getSensorReading();
-                    std::cout << "Sensor readings: ";
-                    for (int reading : readings) {
-                        std::cout << reading << " ";
-                    }
-                    std::cout << std::endl;
+                } else if (isButtonClicked(buttons[4], mousePos)) {
+                    currentSensorReadings = robot.getSensorReading();
                 }
             }
         }
 
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color(10, 10, 10));
         drawMap(window, map, robot);
-        drawButtons(window, font);
+        drawUI(window, font, currentSensorReadings, mousePos);
         window.display();
     }
 
